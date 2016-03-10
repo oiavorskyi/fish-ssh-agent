afad6598ce18fcbe206fed4201efbfb96e25ec3e
Marcel Bischoff <marcel@herrbischoff.com>
2016-03-10 01:59:58 +0100

    Cleanup

--- functions/__ssh-agent.fish
+++ functions/__ssh-agent.fish
@@ -1,6 +1,6 @@
 setenv SSH_ENV $HOME/.ssh/environment
 
-function start_agent                                                                                                                                                                    
+function __start_agent                                                                                                                                                                    
     echo "Initializing new SSH agent ..."
     ssh-agent -c | sed 's/^echo/#echo/' > $SSH_ENV
     echo "succeeded"
@@ -9,12 +9,12 @@ function start_agent
     ssh-add
 end
 
-function test_identities                                                                                                                                                                
+function __test_identities                                                                                                                                                                
     ssh-add -l | grep "The agent has no identities" > /dev/null
     if [ $status -eq 0 ]
         ssh-add
         if [ $status -eq 2 ]
-            start_agent
+            __start_agent
         end
     end
 end
@@ -22,7 +22,7 @@ end
 if [ -n "$SSH_AGENT_PID" ] 
     ps -ef | grep $SSH_AGENT_PID | grep ssh-agent > /dev/null
     if [ $status -eq 0 ]
-        test_identities
+        __test_identities
     end  
 else
     if [ -f $SSH_ENV ]
@@ -30,8 +30,8 @@ else
     end  
     ps -ef | grep $SSH_AGENT_PID | grep -v grep | grep ssh-agent > /dev/null
     if [ $status -eq 0 ]
-        test_identities
+        __test_identities
     else 
-        start_agent
+        __start_agent
     end  
 end
